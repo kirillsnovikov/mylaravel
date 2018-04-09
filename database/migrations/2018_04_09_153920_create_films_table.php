4adef2ef7fd82782baf2756cc52e3b5446622c36
Kirill Novikov <KNovikov@horse21.local>
2018-04-09 19:09:15 +0300

    make migrations

---
+++ database/migrations/2018_04_09_153920_create_films_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('link');
+            $table->text('content');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->integer('status');
+            $table->integer('views');
+            $table->integer('is_featured');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
